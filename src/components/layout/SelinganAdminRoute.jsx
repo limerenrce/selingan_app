200ac55d4e505bdaac5c4e822229ec7f5336318b
Satya Uttami <satyautta@gmail.com>
2024-12-17 16:34:02 +0800

    finish log out

--- src/components/layout/SelinganAdminRoute.jsx
+++ src/components/layout/SelinganAdminRoute.jsx
@@ -1,6 +1,22 @@
+// import { Navigate, useLocation, useNavigate } from 'react-router-dom';
+// import { useAuth } from "../../utils/AuthContext";
 import SelinganAdminLayout from "./AMain";
 // eslint-disable-next-line react/prop-types
-const SelinganAdminRoute = ({ component }) => {
+const SelinganAdminRoute = ({ component,
+  //  role
+   }) => {
+  // const { user } = useAuth();
+  // const location = useLocation();
+  // const navigate = useNavigate();
+
+  // if (!user) {
+  //   return <Navigate to="/login" state={{ from: location }} replace />;
+  // }
+
+  // if (role && user.role !== role) {
+  //   return <Navigate to="/" state={{ from: location }} replace />; // Redirect to a general page or error page
+  // }
+
   return (
     <>
       <SelinganAdminLayout> {component} </SelinganAdminLayout>
--- src/pages/Auth/SignIn.jsx
+++ src/pages/Auth/SignIn.jsx
@@ -75,7 +75,7 @@ const SignIn = () => {
 
     getDataPrivate("/api/v1/protected/data")
       .then((data) => {
-        const userRole = data.role;
+        const userRole = data.user_info["role"];
         if (userRole === "user") {
           navigate("/ragam", { replace: true });
         } else if (userRole === "admin") {
